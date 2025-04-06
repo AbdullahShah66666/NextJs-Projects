eaf82862611c4264bf4ae9938ebe5a64b37e8c5b
Abdullah Shah <shah3152006@gmail.com>
2025-04-06 05:02:43 +0500

    Completed To-Do List Logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,19 @@ const ButtonForPractice = dynamic(
   }
 );*/
 
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-import ToDoList from '../components/to-do-list';
+import ToDoList from "../components/to-do-list";
 
 export default function Home() {
   return (
-    <div className="flex-grow flex flex-col justify-center items-center bg-gray-500">
-      <h1 className="py-2 px-4 my-1 bg-black">
-        <Link href="/products">Products</Link>
+    <div className='flex-grow flex flex-col justify-center items-center bg-gray-500'>
+      <h1 className='py-2 px-4 my-1 bg-black'>
+        <Link href='/products'>Products</Link>
       </h1>
-      <h1 className="py-2 px-4 my-1 bg-black">
-        <Link href="/products">Products to test Flexbox</Link>
+      <h1 className='py-2 px-4 my-1 bg-black'>
+        <Link href='/products'>Products to test Flexbox</Link>
       </h1>
       <ToDoList />
     </div>
--- src/components/to-do-list.tsx
+++ src/components/to-do-list.tsx
@@ -6,6 +6,8 @@ import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
 
 export default function ToDoList() {
   const inputRef = useRef(null);
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
   const [isDisabled, setIsDisabled] = useState(true);
   const [isClicked, setIsClicked] = useState(false);
 
@@ -15,64 +17,87 @@ export default function ToDoList() {
     }
   }, [isDisabled]);
 
-  /*  
   const handleInputChange = (e) => {
     const value = e.target.value;
-    if (value.length > 0) {
-      setIsDisabled(false);
-      setIsClicked(true);
-    }
+    setInputValue(value);
   };
-*/
-  
-  const handleClick = () => {
+
+  const handleAddTask = () => {
     if (inputRef.current) {
       setIsDisabled(false);
     }
     setIsClicked(true);
+
+    if (inputValue.trim() === "") return;
+    setTasks([...tasks, inputValue]);
+    setInputValue("");
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleAddTask();
+    }
   };
 
   const handleBlur = () => {
     setTimeout(() => {
-      //setIsDisabled(true);
       setIsClicked(false);
-    }, 2000);
+    }, 200);
+    setTimeout(() => {
+      setIsDisabled(true);
+    }, 6000);
   };
+
   return (
-    <>
-      <div className='flex flex-col justify-center items-center bg-pink-300 text-black h-auto w-full'>
-        <h1 className='bg-pink-200 text-center text-2xl font-semibold'>
+    <div className='flex flex-col gap-2 justify-center items-center bg-gray-700 h-auto w-full'>
+      <div className='flex flex-col gap-1 justify-center items-center bg-black text-black h-auto w-full'>
+        <h1 className='bg-pink-200 text-center text-2xl font-semibold w-full'>
           To-Do List :-
         </h1>
-        <ul className='bg-red-500'>
-          <li className='list-disc flex flex-row justify-between gap-2'>
-            <span>Learn React</span>
-            <CiCircleMinus className='bg-green-200' size={20} />
-          </li>
-          <li className='list-disc flex flex-row justify-between gap-2'>
-            <span>Learn NextJs</span>
-            <CiCirclePlus className='bg-green-200' size={20} />
-          </li>
+        <div className='flex w-full h-auto justify-center items-center w-full'>
+          <div className='relative'>
+            <input
+              type='text'
+              value={inputValue}
+              ref={inputRef}
+              disabled={isDisabled}
+              onChange={handleInputChange}
+              onBlur={handleBlur}
+              onKeyDown={handleKeyPress}
+              placeholder='Add a new task'
+              className={`placeholder:pl-2 placeholder:text-xs placeholder:text-black placeholder:opacity-30 rounded-2xl bg-gray-100 text-black h-10 w-36 transition-all duration-300 ${isClicked ? "placeholder:opacity-40 bg-white w-64 rounded-3xl" : ""}`}
+            />
+            <CiCirclePlus
+              className='absolute right-1/50 top-1/2 transform -translate-y-1/2 icon-thicker'
+              size={20}
+              color='black'
+              //style={{ strokeWidth: 3 }}
+              onClick={handleAddTask}
+            />
+          </div>
+        </div>
+        <ul className='bg-gray-100 w-auto h-auto p-2 rounded-md w-full'>
+          {tasks.map((task, index) => (
+            <li
+              key={index}
+              className='list-disc flex flex-row justify-between gap-2'
+            >
+              <span className='text-black'>{task}</span>
+              <CiCircleMinus
+                className='bg-white rounded-full icon-thicker'
+                size={20}
+                color='black'
+                onClick={() => handleDeleteTask(index)}
+              />
+            </li>
+          ))}
         </ul>
       </div>
-      <div className='flex w-full h-auto justify-center items-center'>
-        <div className='relative'>
-          <input
-            ref={inputRef}
-            disabled={isDisabled}
-            //onChange={handleInputChange}
-            onBlur={handleBlur}
-            placeholder='Add a new task'
-            className={`bg-green-400 text-black w-36 transition-all duration-300 ${isClicked ? "bg-green-600 w-64" : ""}`}
-          />
-          <CiCirclePlus
-            className='absolute right-1/50 top-1/2 transform -translate-y-1/2'
-            size={20}
-            color='black'
-            onClick={handleClick}
-          />
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
